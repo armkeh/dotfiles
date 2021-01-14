15b7a3f66cc970e73db38a1116a6c77f9b8109a1
Mark Armstrong <markparmstrong@gmail.com>
2021-01-14 14:52:39 -0500

    custom.el changes that have piled up.

--- emacs/custom.el
+++ emacs/custom.el
@@ -3,23 +3,57 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(bibtex-completion-bibliography "~/Dropbox/McMaster/references.bib")
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#000000" "#a80000" "#005f00" "#8b3800" "#0030a6" "#721045" "#005589" "#ffffff"])
+ '(bibtex-completion-bibliography "~/Dropbox/McMaster/references.bib" t)
  '(custom-safe-themes
-   '("6edf762b89c629f7f19571a1da2baae181bed8bb4733630efbee0903094a0b50" "c17157e15c1c1bfbff9692c32ffe22f28d8c85a45f59324c849764edeca69e60" "e074be1c799b509f52870ee596a5977b519f6d269455b84ed998666cf6fc802a" "fa3bdd59ea708164e7821574822ab82a3c51e262d419df941f26d64d015c90ee" "43c808b039893c885bdeec885b4f7572141bd9392da7f0bd8d8346e02b2ec8da" "e838d6375a73fda607820c65eb3ea1f9336be7bd9a5528c9161e10c4aa663b5b" "e3c87e869f94af65d358aa279945a3daf46f8185f1a5756ca1c90759024593dd" default))
+   '("00c76aa2e529fbfaf53f94806a9245437530517bd5586ab75501eba0930ad050" "a8e4738b03da5e8c5770c17b9fb9d20a1b305b117ed4e035c0b6e4cb6f291a2b" "5e6543bb81a38dc8ab98129f1a40bf64c329d2224a1c5300b41a7a6dea3cfa3a" "397f58254bd7808f68e38e0a812ce452e79f759d46b9e2ccd319cabe8a83add9" "88380a535b965f1172ced30e751f5abf31047f15eae17adf323ba415a9408617" "98da47acc5b1078e3eb222e3020579efd85e5fdcc24bde86a285ee472606a734" "cd4a50c11d395cb67bae52e94606e8098c82c4397d2ebacca7ec37e4f47d673c" "70292d08825e9c07a3d9517b99d9a944b30957c8aa18be2c0a019595ad049a3a" "9371bb1d80e91807157958a847e18712e5043af7563a055ad03904c2d90e5078" "6edf762b89c629f7f19571a1da2baae181bed8bb4733630efbee0903094a0b50" "c17157e15c1c1bfbff9692c32ffe22f28d8c85a45f59324c849764edeca69e60" "e074be1c799b509f52870ee596a5977b519f6d269455b84ed998666cf6fc802a" "fa3bdd59ea708164e7821574822ab82a3c51e262d419df941f26d64d015c90ee" "43c808b039893c885bdeec885b4f7572141bd9392da7f0bd8d8346e02b2ec8da" "e838d6375a73fda607820c65eb3ea1f9336be7bd9a5528c9161e10c4aa663b5b" "e3c87e869f94af65d358aa279945a3daf46f8185f1a5756ca1c90759024593dd" default))
+ '(default-input-method "Agda")
  '(fci-rule-color "#62686E")
+ '(flymake-error-bitmap '(flymake-double-exclamation-mark modus-theme-fringe-red))
+ '(flymake-note-bitmap '(exclamation-mark modus-theme-fringe-cyan))
+ '(flymake-warning-bitmap '(exclamation-mark modus-theme-fringe-yellow))
+ '(highlight-tail-colors '(("#aecf90" . 0) ("#c0efff" . 20)))
+ '(hl-todo-keyword-faces
+   '(("HOLD" . "#714900")
+     ("TODO" . "#721045")
+     ("NEXT" . "#5317ac")
+     ("THEM" . "#8f0075")
+     ("PROG" . "#005589")
+     ("OKAY" . "#185870")
+     ("DONT" . "#305c00")
+     ("FAIL" . "#a80000")
+     ("DONE" . "#005f00")
+     ("NOTE" . "#804000")
+     ("KLUDGE" . "#8b3800")
+     ("HACK" . "#8b3800")
+     ("TEMP" . "#4d0006")
+     ("FIXME" . "#a0132f")
+     ("XXX+" . "#972500")
+     ("REVIEW" . "#005a68")
+     ("DEPRECATED" . "#001170")))
+ '(ibuffer-deletion-face 'dired-flagged)
+ '(ibuffer-filter-group-name-face 'dired-mark)
+ '(ibuffer-marked-face 'dired-marked)
+ '(ibuffer-title-face 'dired-header)
  '(jdee-db-active-breakpoint-face-colors (cons "#1c1f24" "#51afef"))
  '(jdee-db-requested-breakpoint-face-colors (cons "#1c1f24" "#7bc275"))
  '(jdee-db-spec-breakpoint-face-colors (cons "#1c1f24" "#484854"))
  '(objed-cursor-color "#ff665c")
- '(org-ref-default-bibliography '("~/Dropbox/McMaster/references.bib"))
  '(package-selected-packages
-   '(counsel swiper xah-fly-keys modus-operendi modus-vivendi modus-operandi-theme modus-vivendi-theme all-the-icons helm auto-package-update toc-org typescript-mode rainbow-delimiters dimmer dimmer-mode auto-dim-other-buffers all-the-icons-dired org-bullets doom-modeline spaceline spaceline-config helm-bibtex org-ref unicode-fonts geiser yasnippet yankpad wordsmith-mode which-key use-package undo-tree racket-mode pdf-tools ox-tufte ox-reveal ox-pandoc org-plus-contrib org-mime magit htmlize general exwm emojify doom-themes dired-single diminish))
+   '(forge cider inf-ruby ob-prolog clojure-mode haskell-mode org zones el-get hl-todo magit company-quickhelp company-emoji company mu4e-maildirs-extension elm-mode docker ob-ammonite web-mode ob-typescript command-log-mode messages-are-flowing polymode ivy evil counsel swiper xah-fly-keys modus-operendi modus-vivendi modus-operandi-theme modus-vivendi-theme all-the-icons helm auto-package-update toc-org typescript-mode rainbow-delimiters dimmer dimmer-mode auto-dim-other-buffers all-the-icons-dired org-bullets doom-modeline spaceline spaceline-config unicode-fonts geiser yasnippet yankpad wordsmith-mode which-key use-package undo-tree pdf-tools ox-tufte ox-reveal ox-pandoc org-plus-contrib org-mime htmlize general exwm emojify doom-themes dired-single diminish))
  '(pdf-view-midnight-colors (cons "#bbc2cf" "#242730"))
- '(reftex-default-bibliography '("~/Dropbox/McMaster/references.bib"))
+ '(reftex-default-bibliography '("~/Dropbox/McMaster/references.bib") t)
  '(rustic-ansi-faces
    ["#242730" "#ff665c" "#7bc275" "#FCCE7B" "#51afef" "#C57BDB" "#5cEfFF" "#bbc2cf"])
  '(safe-local-variable-values
    '((eval add-hook 'after-save-hook
+           (lambda nil
+             (org-babel-tangle))
+           nil t)
+     (eval add-hook 'after-save-hook
            (lambda nil
              (save-to-drive "org"))
            nil 'local)
@@ -27,6 +61,7 @@
  '(toc-org-noexport-regexp
    "\\(^*+\\) +.*:\\(ignore\\|noexport\\)\\([@_][0-9]\\)?:\\($\\|[^ ]*?:$\\)" t)
  '(vc-annotate-background "#242730")
+ '(vc-annotate-background-mode nil)
  '(vc-annotate-color-map
    (list
     (cons 20 "#7bc275")
@@ -47,7 +82,11 @@
     (cons 320 "#754f56")
     (cons 340 "#62686E")
     (cons 360 "#62686E")))
- '(vc-annotate-very-old-color nil))
+ '(vc-annotate-very-old-color nil)
+ '(xterm-color-names
+   ["#000000" "#a80000" "#005f00" "#8b3800" "#0030a6" "#721045" "#005589" "#f3f1f3"])
+ '(xterm-color-names-bright
+   ["#505050" "#972500" "#305c00" "#714900" "#223fbf" "#8f0075" "#185870" "#ffffff"]))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
